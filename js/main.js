560b1822f17ec0b8f13f85784c493e47e5f7d675
nashEm8 <emanuel82022@outlook.com>
2023-01-04 21:22:50 -0300

    Adicionei a função translation que faz uma requisição, depois criei um evento de click no button 'traduzir', onde ele vai executar a função translation e criei um evento de click no button 'delete', onde vai apagar os dados dentro dos textarea

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
+const selects = document.querySelectorAll('select');
 const textAreaFrom = document.querySelector('#textAreaFrom');
 const textAreaTo = document.querySelector('#textAreaTo');
 const btnTranslate = document.querySelector('#btnTranslate');
-const selects = document.querySelectorAll('select');
+const btnDelete = document.querySelector('#btnDelete');
 
 const contries = {
     'en-GB' : 'Inglês',
@@ -14,14 +15,40 @@ const contries = {
 selects.forEach((tag) => {
     for(let country in contries){
         let selected;
-        if(tag.className.includes('selectFrom') && country === 'pt-BR'){
-            selected = 'selected';
-        } else if(tag.className.includes('selectTo') && country === 'en-GB'){
-            selected = 'selected';
-        }
-
-        const option = `<option value="${country}" ${selected}>${contries[country]}</option>`
+        tag.className.includes('selectFrom') && country === 'pt-BR' ? selected = 'selected' : '';
+        tag.className.includes('selectTo') && country === 'en-GB' ? selected = 'selected' : '';
         
+        const option = `<option value="${country}" ${selected}>${contries[country]}</option>`
         tag.insertAdjacentHTML('beforeend', option)   
     }
 });
+
+
+
+
+btnTranslate.addEventListener('click', () => {
+    textAreaFrom.value ? translation() : alert('Digite o texto ou palavra que deseja traduzir!');
+});
+
+btnDelete.addEventListener('click', () => {
+    if(!(textAreaFrom.value === '') && !(textAreaTo.value === '')){
+        textAreaFrom.value = '';
+        textAreaTo.value = '';
+    } else if((textAreaFrom.value === '') && (textAreaTo.value === '')){
+        alert('Não há nenhum dado inserido para ser apagado!')
+    }
+});
+
+const translation = () => {
+const baseUrl = `https://api.mymemory.translated.net/get?q=`;
+const url = `${baseUrl}${textAreaFrom.value}&langpair=${selects[0].value}|${selects[1].value}`;
+
+    const diceTranslator = async () => await fetch(url).then(response => response.json());
+
+    const showTranslation = async () => {
+        const showResponse = await diceTranslator();
+        textAreaTo.textContent = showResponse.responseData.translatedText;
+    }   
+
+    showTranslation()
+}
